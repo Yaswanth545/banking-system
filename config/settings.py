9ff17a93de72238775b63cbddc691fa6eac31522
Yaswanth Kumar <yaswanthkumargurram@gmail.com>
2026-08-03 15:15:33 +0530

    Add Docker support, Redis cache, PostgreSQL container and account statement export

--- config/settings.py
+++ config/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": config("REDIS_URL"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
